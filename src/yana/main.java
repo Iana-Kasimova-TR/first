f0f3b1bc3cefd22dc9c5db4a480a08453ab615cc
anakasimova <mikkimous555@gmail.com>
2016-10-11 14:39:41 +0300

    Hello)

--- src/yana/main.java
+++ src/yana/main.java
@@ -6,6 +6,6 @@ package yana;
 public class main {
     public static void main(String[] args) {
         // Display "Hello World!"
-        System.out.println("Hello World!");
+        System.out.println("Hi World!");
     }
 }
