44c95327ccd8622a3151bf0b6a7f52f94edb70e6
anakasimova <mikkimous555@gmail.com>
2016-10-11 14:28:54 +0300

    Hello)

---
+++ src/yana/main.java
@@ -0,0 +1,11 @@
+package yana;
+
+/**
+ * Created by anakasimova on 10/10/16.
+ */
+public class main {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+        System.out.println("Hello World!");
+    }
+}
